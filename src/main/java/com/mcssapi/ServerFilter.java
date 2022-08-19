964db4ef7cd33847b6ab89d105dfe4292c9c715e
Filliravaz <40716956+vaio2005@users.noreply.github.com>
2022-08-19 18:39:17 +0200

    Rename ServerStatus enum to ServerFilter

---
+++ src/main/java/com/mcssapi/ServerFilter.java
@@ -0,0 +1,17 @@
+package com.mcssapi;
+
+public enum ServerFilter {
+
+    NONE(0),
+    ONLINE(1),
+    OFFLINE(2),
+    FILTER(3);
+
+    private final int value;
+
+    ServerFilter(final int newValue) {
+        value = newValue;
+    }
+
+    public int getValue() { return value; }
+}
