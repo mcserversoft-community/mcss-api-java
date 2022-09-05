2ac62a4c7bdef46467eed869b46a9b50d1654097
Filliravaz <40716956+vaio2005@users.noreply.github.com>
2022-09-05 23:44:23 +0200

    First JUnit Tests

--- src/test/java/dev/le_app/MCSSApiTest.java
+++ src/test/java/dev/le_app/MCSSApiTest.java
@@ -1,8 +1,11 @@
 package dev.le_app;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import dev.le_app.exceptions.APINotFoundException;
+import dev.le_app.exceptions.APIUnauthorizedException;
+import dev.le_app.exceptions.APIVersionMismatchException;
+import org.junit.jupiter.api.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,16 +17,78 @@ class MCSSApiTest {
     Scheduler scheduler;
     ArrayList<Task> tasks;
     Job job;
+    Info info;
 
-    String ip;
-    String token;
+    static String ip;
+    static String token;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void setUp() {
         //Get the ip from the gradle build file
-        ip =  
-
+        ip = System.getProperty("ip");
+        token = System.getProperty("token");
     }
+
+    @Test
+    @DisplayName("Test the API connection")
+    @Order(1)
+    void testConnection() {
+        try {
+            api = new MCSSApi(ip, token);
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Thorn IOException while testing connection");
+        } catch (APIUnauthorizedException e) {
+            e.printStackTrace();
+            fail("Thorn APIUnauthorizedException while testing connection");
+        } catch (APIVersionMismatchException e) {
+            e.printStackTrace();
+            fail("Thorn APIVersionMismatchException while testing connection");
+        }
+        assertNotNull(api, "API is null");
+    }
+
+    @Test
+    @DisplayName("Testing returned information about API")
+    @Order(2)
+    void testInformation() {
+        try {
+            info = api.getInfo();
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Thorn IOException while testing information");
+        } catch (APIUnauthorizedException e) {
+            e.printStackTrace();
+            fail("Thorn APIUnauthorizedException while testing information");
+        }
+        assertNotNull(info, "Info is null");
+        assertEquals("1.0.0", info.getMCSSApiVersion(), "Version is not 1.0.0");
+        assertTrue(info.getYouAreAwesome(), "Expected \"areYouAwesome\" to be true");
+    }
+
+    @Test
+    @DisplayName("Get Servers")
+    @Order(3)
+    void testGetServers() {
+        try {
+            servers = api.getServers();
+        } catch (APIUnauthorizedException e) {
+            e.printStackTrace();
+            fail("Thrown ApiUnauthorizedException while getting servers");
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Thrown IOException while getting servers");
+        } catch (APINotFoundException e) {
+            e.printStackTrace();
+            fail("Thrown APINotFoundException while getting servers");
+        }
+        assertNotNull(servers, "Servers is null");
+        assertTrue(servers.size() > 0, "Servers is empty");
+        assertNotNull(servers.get(0).getGUID(), "Server GUID is null");
+    }
+
+    @Test
+
 
     @AfterEach
     void tearDown() {
