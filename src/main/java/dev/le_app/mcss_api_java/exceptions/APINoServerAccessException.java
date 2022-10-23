8f404a5bf9275c8daedd38db9e5509c8e7591986
Filliravaz <40716956+vaio2005@users.noreply.github.com>
2022-10-23 16:26:22 +0200

    Added APINoServerAccessException
    Thrown when api key is missing rights to access a specific function

---
+++ src/main/java/dev/le_app/mcss_api_java/exceptions/APINoServerAccessException.java
@@ -0,0 +1,7 @@
+package dev.le_app.mcss_api_java.exceptions;
+
+public class APINoServerAccessException extends Exception {
+    public APINoServerAccessException(String message) {
+        super(message);
+    }
+}
